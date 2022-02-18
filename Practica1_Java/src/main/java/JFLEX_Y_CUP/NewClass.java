7bff59c6186bcf1c82909d3cf7fdbdf40763a26e
Alex-MirandaS <williammiranda201930967@cunoc.edu.gt>
2022-02-18 03:59:05 -0600

    Creación de Proyecto en Java

---
+++ Practica1_Java/src/main/java/JFLEX_Y_CUP/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JFLEX_Y_CUP;
+
+/**
+ *
+ * @author alex
+ */
+public class NewClass {
+    
+}
